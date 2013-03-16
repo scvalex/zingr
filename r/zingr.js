98ebcce5c7da59da77c93c3d0f31c3c10957b078
Alexandru Scvortov <scvalex@gmail.com>
2013-03-16 01:48:13 +0000

    add feed button

--- r/zingr.js
+++ r/zingr.js
@@ -1,3 +1,38 @@
+function log() {
+    if (console && console.log) {
+        console.log.apply(console.log, arguments);
+    }
+}
+
+function Feed(title, url) {
+    var self = this;
+
+    self.title = title;
+    self.url = url;
+}
+
+function AppViewModel() {
+    var self = this;
+
+    self.feeds = ko.observable([new Feed("alex", "alexUrl"), new Feed("max", "maxUrl")]);
+    self.addingFeed = ko.observable(false);
+    self.newFeedUrl = ko.observable("");
+
+    self.addFeedClicked = function(e) {
+        self.addingFeed(!self.addingFeed());
+        if (self.addingFeed()) {
+            self.newFeedUrl("");
+        } else {
+            self.addFeed(self.newFeedUrl());
+        }
+    }
+
+    self.addFeed = function(url) {
+        log("Adding feed: ", url);
+    }
+}
+
 document.addEvent("domready", function() {
-    alert("document loaded");
+    ko.applyBindings(new AppViewModel());
+    log("document loaded");
 });
